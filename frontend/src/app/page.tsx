8687678d34b202140156f4dfab47394c206092ba
Sofia <m.sofiasarhiri@gmail.com>
2024-09-30 16:29:07 -0500

    triange background and centered components on main page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -16,21 +16,26 @@ const HomePage: React.FC = () => {
   }, []);
 
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center">
-      <div className="flex flex-col items-center rounded-lg border border-gray-300 p-20 text-center shadow-2xl">
-        <h1 className="mb-6 text-3xl font-bold">Welcome to Taco Finder</h1>
-        <Header
-          onLocationShare={(loc: Location) => handleLocationShare(loc, router)}
-          onAddressSubmit={(address: string) =>
-            handleAddressSubmit(address, router)
-          }
-        />
-
-        <p>
-          This is the landing page. Use the search functionality to find tacos!
-        </p>
-      </div>
+    <div className="flex min-h-screen flex-col items-center justify-center relative bg-white">
+    {/* Triangle Background */}
+    <div className="absolute inset-0">
+      <div className="absolute top-0 left-0 w-0 h-0 border-t-[100vh] border-t-[#eece30] border-r-[100vw] border-r-transparent"></div>
     </div>
+
+    {/* Content */}
+    <div className="relative z-10 flex flex-col items-center rounded-lg border border-gray-300 p-20 text-center shadow-2xl bg-white">
+      <h1 className="mb-6 text-6xl font-brothers">Welcome to Taco Finder</h1>
+      <Header
+        onLocationShare={(loc: Location) => handleLocationShare(loc, router)}
+        onAddressSubmit={(address: string) =>
+          handleAddressSubmit(address, router)
+        }
+      />
+      <p>
+        This is the landing page. Use the search functionality to find tacos!
+      </p>
+    </div>
+  </div>
   );
 };
 
