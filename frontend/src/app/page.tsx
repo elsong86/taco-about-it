427c743d4d8fd1be4e214334eb41715b22fed8bf
Sofia <m.sofiasarhiri@gmail.com>
2024-09-30 18:18:10 -0500

    Header component main styling done

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -24,42 +24,42 @@ const HomePage: React.FC = () => {
 
     <div>
       <MainHead />
-    <div className="flex min-h-screen flex-col items-center justify-center relative"
-    style={{
-      backgroundImage: `url(${image.src})`,
-      backgroundSize: "cover",
-      backgroundPosition: "contain", 
-      backgroundRepeat: "no-repeat", 
-    }}
-    >
-      
+      <div className="flex min-h-screen flex-col items-center justify-center relative"
+        style={{
+          backgroundImage: `url(${image.src})`,
+          backgroundSize: "cover",
+          backgroundPosition: "contain", 
+          backgroundRepeat: "no-repeat", 
+        }}
+        >
+          
 
-    {/* Triangle Background */}
-    {/* <div className="absolute inset-0">
-      <div className="absolute top-0 left-0 w-0 h-0 border-t-[100vh] border-t-[#eece30] border-r-[100vw] border-r-transparent"></div>
+        {/* Triangle Background */}
+        {/* <div className="absolute inset-0">
+          <div className="absolute top-0 left-0 w-0 h-0 border-t-[100vh] border-t-[#eece30] border-r-[100vw] border-r-transparent"></div>
 
-    </div> */}
-      
+        </div> */}
+          
 
-    {/* Content */}
-    <div className="relative z-10 flex flex-col items-center rounded-lg border border-gray-300 p-10 text-center shadow-2xl bg-transparent mt-10">
-    <p className="font-thirsty text-3xl pb-0 ">
-       ~ Anywhere, Anytime ~
-          </p>
-      <h1 className=" text-6xl font-brothers textShadow-lg" >FIND YOUR NEW FAVORITE <br />TACO SPOT</h1>
-      <p className='font-semibold '>
-            Enter your location to get started. 
-          </p>
-      <Header
-        onLocationShare={(loc: Location) => handleLocationShare(loc, router)}
-        onAddressSubmit={(address: string) =>
-          handleAddressSubmit(address, router)
-        }
-      />
-      
+        {/* Content */}
+      <div className="relative z-10 flex flex-col items-center rounded-lg border border-gray-300 p-10 text-center shadow-2xl bg-gray-200 bg-opacity-50 mt-10">
+        <p className="font-thirsty text-3xl pb-0 ">
+          ~ Anywhere, Anytime ~
+              </p>
+          <h1 className=" text-6xl font-brothers textShadow-lg" >FIND YOUR NEW FAVORITE <br />TACO SPOT</h1>
+          <p className='font-semibold '>
+                Enter your location to get started. 
+              </p>
+          <Header
+            onLocationShare={(loc: Location) => handleLocationShare(loc, router)}
+            onAddressSubmit={(address: string) =>
+              handleAddressSubmit(address, router)
+            }
+          />
+        </div>
+
+      </div>
     </div>
-  </div>
-  </div>
   );
 };
 
